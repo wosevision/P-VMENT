fe3e6bdc9b347615f86ffbb8e5ce9f879b11d342
wosevision <jacksonteather@hotmail.com>
2016-01-22 08:41:02 -0500

    Added "Hill Finder" page
    Includes Flickity directive for “ride style” icons

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,25 +1,47 @@
 (function() {
   'use strict';
 
-  var app = angular.module('application', [
+  angular.module('application', [
     'ui.router',
     'ngAnimate',
 
     //foundation
     'foundation',
     'foundation.dynamicRouting',
-    'foundation.dynamicRouting.animations'
+    'foundation.dynamicRouting.animations',
+
+    //flickity
+    'bc.Flickity'
   ])
+    .controller('hillFinder', hillFinder)
+    .service('UserService')
     .config(config)
     .run(run)
-    .controller('hillFinder', hillFinder)
   ;
 
-  hillFinder.$inject = ['$scope', '$log', '$state'];
-  function hillFinder ($scope, $log, $state) {
-    $scope.intensities = {
-      0:["Flat","color-darkgrey"],1:["Flat","color-grrey"], 2:["Leisure","color-lightblue"], 3:["Leisure","color-blue"], 4:["Cruise","color-green"], 5:["Cruise","color-lightgreen"], 6:["Shred","color-yellow"], 7:["Shred","color-lightorange"], 8:["Bomb","color-orange"], 9:["Bomb","color-red"], 10:["Kamikaze","color-brand"]
-    };
+  hillFinder.$inject = ['$scope'];
+  function hillFinder ($scope) {
+    $scope.intensities = ["Flat", "Leisure", "Cruise", "Shred", "Bomb", "Kamikaze"];
+    $scope.getIntensity = function(value) {
+      if (value < 20) { return $scope.intensities[0]; }
+      else if (value >= 20 && value < 40) { return $scope.intensities[1]; }
+      else if (value >= 40 && value < 60) { return $scope.intensities[2]; }
+      else if (value >= 60 && value < 80) { return $scope.intensities[3]; }
+      else if (value >= 80 && value < 100) { return $scope.intensities[4]; }
+      else if (value == 100) { return $scope.intensities[5]; }
+    }
+    $scope.slides = [
+        { 'path' : 'assets/img/bomb.svg',
+          'label' : 'Bomb'},
+        { 'path' : 'assets/img/carve.svg',
+          'label' : 'Carve'},
+        { 'path' : 'assets/img/distance.svg',
+          'label' : 'Distance'},
+        { 'path' : 'assets/img/trick.svg',
+          'label' : 'Trick'},
+        { 'path' : 'assets/img/slide.svg',
+          'label' : 'Slide'}
+    ];
   }
 
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
