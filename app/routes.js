de1a448a2eef95461c60da300626b1f3951ce001
wosevision <jacksonteather@hotmail.com>
2016-01-23 01:40:34 -0500

    Add GET and PUT routes
    /users endpoint can retrieve and update users by uid

--- app/routes.js
+++ app/routes.js
@@ -14,10 +14,10 @@ var User = require('./models/user');
         });
 
         router.route('/users')
-            // create a bear (accessed at POST http://localhost:8080/api/bears)
+            // create a user (accessed at POST http://localhost:8080/api/users)
             .post(function(req, res) {
                 
-                var user = new User();      // create a new instance of the Bear model
+                var user = new User();      // create a new instance of the Usar model
                 user.firstName = req.body.firstName;  // set the users name (comes from the request)
                 user.lastName = req.body.lastName;
                 user.email = req.body.email; 
@@ -38,7 +38,49 @@ var User = require('./models/user');
                     res.json(users);
                 });
             });
-        // more routes for our API will happen here
+
+        router.route('/users/:user_id')
+            // get the user with that id (accessed at GET http://localhost:8080/api/users/:user_id)
+            .get(function(req, res) {
+                User.findById(req.params.user_id, function(err, user) {
+                    if (err)
+                        res.send(err);
+                    res.json(user);
+                });
+            })
+            .put(function(req, res) {
+                // use our user model to find the user we want
+                User.findById(req.params.user_id, function(err, user) {
+
+                    if (err)
+                        res.send(err);
+
+                    user.firstName = req.body.firstName;  // set the users name (comes from the request)
+                    user.lastName = req.body.lastName;
+                    user.email = req.body.email;
+
+                    // save the user
+                    user.save(function(err) {
+                        if (err)
+                            res.send(err);
+
+                        res.json({ message: 'User updated!' });
+                    });
+
+                });
+            })
+            // delete the user with this id (accessed at DELETE http://localhost:8080/api/users/:user_id)
+            .delete(function(req, res) {
+                User.remove({
+                    _id: req.params.user_id
+                }, function(err, user) {
+                    if (err)
+                        res.send(err);
+
+                    res.json({ message: 'Successfully deleted' });
+                });
+            });
+
 
         // REGISTER OUR ROUTES -------------------------------
         // all of our routes will be prefixed with /api
--- gulpfile.js
+++ gulpfile.js
@@ -180,19 +180,19 @@ gulp.task('server', ['build'], function() {
     hideFields: ['__v']
   }));
 
-  // app.use(bodyParser.json()); 
+  app.use(bodyParser.json()); 
 
   // // parse application/vnd.api+json as json
-  // app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+  app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
   // // parse application/x-www-form-urlencoded
-  // app.use(bodyParser.urlencoded({ extended: true })); 
+  app.use(bodyParser.urlencoded({ extended: true })); 
 
   // // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-  // app.use(methodOverride('X-HTTP-Method-Override')); 
+  app.use(methodOverride('X-HTTP-Method-Override')); 
 
   // // routes ==================================================
-  // require('./app/routes')(app); // configure our routes
+  require('./app/routes')(app); // configure our routes
 
   var server = app.listen(8079, function() {
     mock.init();
